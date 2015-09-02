d71313f4f92c4e32c2c70385352d5b27d231069b
Corey Powell <mistdragon100@gmail.com>
2015-09-01 19:14:51 -0500

    Bumped version to 1.8.0

--- lib/dragontk/version.rb
+++ lib/dragontk/version.rb
@@ -1,6 +1,6 @@
 module DragonTK
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 1, 7, 0, nil
+    MAJOR, MINOR, TEENY, PATCH = 1, 8, 0, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join(".").freeze
   end
   VERSION = Version::STRING
